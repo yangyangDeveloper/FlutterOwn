d353ffe9bbcfeb51a97b9201f8313b05adf9355f
yangyangDeveloper <zyydeveloper@163.com>
2022-04-20 15:11:43 +0800

    修改列表

--- flutter_zyy/lib/list3.dart
+++ flutter_zyy/lib/list3.dart
@@ -1,4 +1,3 @@
-
 import 'dart:ui';
 import 'package:flutter/material.dart';
 
@@ -20,7 +19,6 @@ class MessageItem implements ListItem {
 }
 
 class Box extends StatelessWidget {
-
   String index = '1';
 
   Box(String index) {
@@ -31,21 +29,20 @@ class Box extends StatelessWidget {
     return Container(
       width: 200,
       height: 50,
-      decoration: BoxDecoration (
+      decoration: BoxDecoration(
         color: Colors.transparent,
       ),
       alignment: Alignment.center,
       child: Text(
-          index,
-          style: TextStyle(
-            color: Colors.white,
-             fontSize: 14,
-          ),
+        index,
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 14,
+        ),
       ),
     );
   }
 }
-
 
 class list3 extends StatelessWidget {
   list3({Key? key}) : super(key: key);
@@ -65,28 +62,35 @@ class list3 extends StatelessWidget {
   // 头部
   Widget _headingItem(BuildContext context, int index) {
     final item = items[index];
-
+    
     if (item is HeadingItem) {
-      return Container (
-        //height: 40,
-        alignment: Alignment.center,
-        decoration: const BoxDecoration(
+      // ListTile(
+      //   // 其实有默认值 好像是15
+      //   //contentPadding: EdgeInsets.symmetric(horizontal: 0),
+      //     title: Expanded(
+      //       child:Text(
+      //         item.heading,
+      //         style: TextStyle(
+      //             color: Colors.grey,
+      //             fontSize: 11,
+      //             letterSpacing: 1
+      //         ),
+      //       ),
+      //     )
+      // ),
+      return Container(
+          // width: 320,
+          height: 30,
+          padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
             color: Color.fromARGB(255, 247, 247, 247),
-        ),
-        child: ListTile(
-            // 其实有默认值 好像是15
-            //contentPadding: EdgeInsets.symmetric(horizontal: 0),
-            textColor: Colors.red,
-            title: Text(
-              item.heading,
-              style: TextStyle(
-                  color: Colors.grey,
-                  fontSize: 11,
-                  letterSpacing: 1
-              ),
-            ),
           ),
-        );
+          child: Text(
+            item.heading,
+            style:
+                TextStyle(color: Colors.grey, fontSize: 11, letterSpacing: 1),
+          ));
     }
     return Container();
   }
@@ -95,42 +99,43 @@ class list3 extends StatelessWidget {
   Widget _messageItem(BuildContext context, int index) {
     final item = items[index];
     if (item is MessageItem) {
-      return  Container(
-        //height: 100,
+      return Container(
+        height: 80,
+        //color: Colors.red,
         //alignment: Alignment.center,
-        decoration: BoxDecoration(
-          color: Colors.white
-        ),
-        child: ListTile(
-          title: Text(item.title,
-            style: TextStyle(
-                color: Colors.grey,
-                fontSize: 14,
-                letterSpacing: 1
+        padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
+        decoration: BoxDecoration(color: Colors.white),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              item.title,
+              style: const TextStyle(
+                    color: Colors.grey, fontSize: 14, letterSpacing: 1),
+              ),
+
+            SizedBox(height: 5,),
+            Text(
+              item.body,
+              style:
+              TextStyle(color: Colors.black, fontSize: 18, letterSpacing: 1),
             ),
-          ),
-          subtitle: Text(item.body,
-            style: TextStyle(
-                color: Colors.black,
-                fontSize: 18,
-                letterSpacing: 1
-            ),
-          ),
-        ),
+          ],
+        )
       );
     }
     return Container();
   }
 
   // 头像
-  Widget _iconItem(){
-
-    return CircleAvatar (
+  Widget _iconItem() {
+    return CircleAvatar(
       radius: 50,
       backgroundImage: AssetImage("assets/16502640932167.jpg"),
       child: Stack(
         children: [
-          Align (
+          Align(
             alignment: Alignment.bottomRight,
             child: CircleAvatar(
               radius: 15,
@@ -145,7 +150,6 @@ class list3 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final size = MediaQuery.of(context).size;
     final width = size.width;
     final height = size.height;
@@ -156,14 +160,14 @@ class list3 extends StatelessWidget {
         title: "1111",
         home: Scaffold(
             appBar: null,
-            body: Padding (
+            body: Padding(
               padding: const EdgeInsets.fromLTRB(0, 0, 0, 0),
-              child: Column (
+              child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget> [
+                children: <Widget>[
                   Container(
                     decoration: BoxDecoration(
-                        color: c1,
+                      color: c1,
                     ),
                     width: width,
                     height: 300,
@@ -171,14 +175,17 @@ class list3 extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         _iconItem(),
-                        Box('张洋洋'),
-                        ],
-                      ),
+                        SizedBox(height: 20,),
+                        Text('张洋洋', style: TextStyle(color: Colors.white),),
+                      ],
                     ),
+                  ),
                   //Divider(height: 100,color: Colors.white),
-                  Expanded(  //解决 Column 中包含ListView 和其他 Container时候  ListView不显示的问题
+                  Expanded(
+                    //解决 Column 中包含ListView 和其他 Container时候  ListView不显示的问题
                     flex: 1,
-                    child: MediaQuery.removePadding( //解决listview顶部有个空白的问题。ListView 上默认的的 top-padding
+                    child: MediaQuery.removePadding(
+                      //解决listview顶部有个空白的问题。ListView 上默认的的 top-padding
                       context: context,
                       removeTop: true,
                       child: ListView.builder(
@@ -187,8 +194,7 @@ class list3 extends StatelessWidget {
                           final item = items[index];
                           if (item is HeadingItem) {
                             return _headingItem(context, index);
-                          }
-                          else if (item is MessageItem) {
+                          } else if (item is MessageItem) {
                             return _messageItem(context, index);
                           }
                           return Container();
@@ -198,9 +204,6 @@ class list3 extends StatelessWidget {
                   ),
                 ],
               ),
-            )
-        )
-    );
+            )));
   }
 }
-
