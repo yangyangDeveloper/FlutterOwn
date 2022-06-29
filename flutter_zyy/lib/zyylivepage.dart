54510f05e650ce8759d3ee053dba4b01b697ea96
yangyangDeveloper <zyydeveloper@163.com>
2022-06-29 11:54:06 +0800

    修改UI

--- flutter_zyy/lib/zyylivepage.dart
+++ flutter_zyy/lib/zyylivepage.dart
@@ -3,7 +3,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
 import 'persistentHeader.dart';
 
-const APPBAR_SCROLL_OFFSET=150;
+const APPBAR_SCROLL_OFFSET = 150;
 
 // 生活tab
 class zyylivepage extends StatefulWidget {
@@ -14,148 +14,230 @@ class zyylivepage extends StatefulWidget {
 }
 
 class _zyylivepageState extends State<zyylivepage> {
-
-  double appBarAlpha = 0;
-
-  _onScroll(offset) {
-    double alpha = offset / APPBAR_SCROLL_OFFSET;
-    if (alpha < 0) {
-      alpha = 0;
-    } else if (alpha > 1) {
-      alpha = 1;
-    }
-    setState(() {
-      appBarAlpha = alpha;
-    });
-  }
-
   List _imageurls = [
     'https://i.ibb.co/ym2R1R7/child-river-dreams-127495-1920x1080.jpg',
     'https://i.ibb.co/FBp1LW8/girl-umbrella-rain-151317-1280x720.jpg',
     'https://i.ibb.co/Yk8C22T/wallhaven-2el5zg.jpg',
   ];
 
+  //late TabController _tabController;
+
+  @override
+  void initState() {
+    //_tabController = TabController(vsync: this, length: 2);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final physicalWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-        body: Stack(
-          children:<Widget> [
-            // 通知
-            MediaQuery.removePadding(  // 用来去除顶部状态栏
-              removeTop: true,
-              context: context,
-              child: NotificationListener(  // 用来检测滚动距离
-                // ignore: missing_return
-                onNotification: (scrollNotification) {
-
-                  // switch (scrollNotification.runtimeType){
-                  //   case ScrollStartNotification: print("开始滚动"); break;
-                  //   case ScrollUpdateNotification: print("正在滚动"); break;
-                  //   case ScrollEndNotification: print("滚动停止"); break;
-                  //   case OverscrollNotification: print("滚动到边界"); break;
-                  // }
-
-                  if(scrollNotification != null && scrollNotification is ScrollUpdateNotification &&
-                      scrollNotification.depth == 0){
-                    print(scrollNotification.metrics.pixels);
-                    _onScroll(scrollNotification.metrics.pixels);
-                    return true;
-                  }
-
-                  return false;
-                },
-                child:CustomScrollView(
-                  slivers: <Widget>[
-
-                    // 轮播包装称sliver
-                    SliverToBoxAdapter(
-                      child: Container(
-                          height: 250,
-                          child: Swiper(
-                            itemCount: _imageurls.length,
-                            autoplay: true,
-                            itemBuilder: (BuildContext context,int index){
-                              return Image.network(
-                                _imageurls[index],
-                                fit: BoxFit.fill,
-                              );
-                            },
-                            pagination: SwiperPagination(),
-                          ),
-                        ),
-                      ),
-
-                    //List  高度相同优先 SliverFixedExtentList  如果不同，使用 SliverList    如果不需要粘连 使用listview
-                    SliverFixedExtentList(
-                      itemExtent: 50.0,
-                      delegate: SliverChildBuilderDelegate(
-                              (BuildContext context, int index) {
-                            //创建列表项
-                            return Container(
-                              alignment: Alignment.center,
-                              color: Colors.lightBlue[100 * (index % 9)],
-                              child: Text('list item $index'),
-                            );
-                          },
-                          childCount: 20
-                      ),
-                    ),
-
-                    // 滚动悬浮固定
-                    SliverPersistentHeader(
-                      pinned: true,
-                      delegate: SliverHeaderDelegate.fixedHeight( //固定高度
-                        height: 150,
-                        child: buildHeader(2),
-                      ),
-                    ),
-
-                    // gridview
-                    SliverPadding(
-                      padding: const EdgeInsets.all(8.0),
-                      sliver: SliverGrid(
-                        //Grid
-                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                          crossAxisCount: 2, //Grid按两列显示
-                          mainAxisSpacing: 10.0,
-                          crossAxisSpacing: 10.0,
-                          childAspectRatio: 4.0,
-                        ),
-                        delegate: SliverChildBuilderDelegate(
-                              (BuildContext context, int index) {
-                            //创建子widget
-                            return Container(
-                              alignment: Alignment.center,
-                              color: Colors.cyan[100 * (index % 9)],
-                              child: Text('grid item $index'),
-                            );
-                          },
-                          childCount: 120,
-                        ),
-                      ),
-                    ),
-                  ],
+      body: CustomScrollView(
+        slivers: <Widget>[
+          SliverAppBar(
+            floating: true,
+            snap: true,
+            pinned: true,
+            expandedHeight: 250,
+            flexibleSpace: FlexibleSpaceBar(
+              title: Text("生活"),
+              titlePadding: EdgeInsets.only(right: 120, bottom: 5),
+              background: Container(
+                height: 250,
+                child: Swiper(
+                  itemCount: _imageurls.length,
+                  autoplay: true,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Image.network(
+                      _imageurls[index],
+                      fit: BoxFit.fill,
+                    );
+                  },
+                  pagination: SwiperPagination(),
                 ),
-
               ),
             ),
+          ),
 
-            // 渐变导航
-            Opacity(
-              opacity: appBarAlpha,
-              child: Container(
-                height: 60,
-                decoration: BoxDecoration(color: Colors.red),
-                child: Center(
-                  child: Padding(padding: EdgeInsets.only(top: 20),
-                    child: Text('首页'),),
-                ),
+          SliverPadding(
+            padding: const EdgeInsets.all(8.0),
+            sliver: SliverGrid(
+              //Grid
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, //Grid按两列显示
+                mainAxisSpacing: 10.0,
+                crossAxisSpacing: 10.0,
+                childAspectRatio: 1, // 设置宽高比
               ),
-            )
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  //创建子widget
+                  var t = index + 1;
 
-          ],
-        )
+                  return Container(
+                    //height: 250,
+                    width: physicalWidth - 20,
+                    alignment: Alignment.center,
+                    color: Colors.red,
+                    child: Image.asset("assets/$t.jpg"),
+                  );
+                },
+                childCount: 4,
+              ),
+            ),
+          ),
+
+          // ListView(
+          //   //itemExtent: 50,
+          //   scrollDirection: Axis.horizontal,
+          //   children: <Widget>[
+          //     new Container(
+          //       width: 160.0,
+          //       color: Colors.red,
+          //     ),
+          //     new Container(
+          //       width: 160.0,
+          //       color: Colors.blue,
+          //     ),
+          //     new Container(
+          //       width: 160.0,
+          //       color: Colors.green,
+          //     ),
+          //     new Container(
+          //       width: 160.0,
+          //       color: Colors.yellow,
+          //     ),
+          //     new Container(
+          //       width: 160.0,
+          //       color: Colors.orange,
+          //     ),
+          //   ],
+          // )
+          //
+
+          //横向滑动区域
+          SliverToBoxAdapter(
+            child: Container(
+                decoration: new BoxDecoration(color: Colors.yellow),
+                width: physicalWidth,
+                height: 300,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text("商品列表2"),
+                      ListTile(title: Text("商品列表")),
+                      SizedBox(
+                        width: physicalWidth, //指定列表高度为400
+                        height: 200,
+                        child: ListView.separated(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 10,
+                          // 设置间隔
+                          separatorBuilder: (BuildContext context, int index) =>
+                              VerticalDivider(
+                            width: 10.0,
+                            color: Color(0xFFFFFFFF),
+                          ),
+                          itemBuilder: (context, index) {
+                            final kScreenWidth =
+                                MediaQuery.of(context).size.width;
+                            final itemWidth = 162.5 / 375 * kScreenWidth;
+                            final itemHeight = 247 / 162.5 * itemWidth;
+                            return Container(
+                              decoration: new BoxDecoration(color: Colors.red),
+                              // width: itemWidth,
+                              // height: itemHeight,
+                              child: Image.asset(
+                                  "assets/life/life_tab_vip_icon.png"),
+                            );
+                          },
+                        ),
+                      ),
+                      Text("商品列表23333"),
+                    ])),
+          ),
+
+          //
+          // SliverPersistentHeader(
+          //   delegate: _SilverAppBarDelegate(TabBar(
+          //     labelColor: Colors.red,
+          //     unselectedLabelColor: Colors.yellow,
+          //     tabs: <Widget>[
+          //       new Tab(
+          //         text: "tab1",
+          //       ),
+          //       new Tab(
+          //         text: "tab2",
+          //       ),
+          //       new Tab(
+          //         text: "tab3",
+          //       ),
+          //     ],
+          //   )),
+          // ),
+
+          // // 滚动悬浮固定
+          SliverPersistentHeader(
+            pinned: true,
+            delegate: SliverHeaderDelegate.fixedHeight(
+              //固定高度
+              height: 80,
+              child: buildHeader(2),
+            ),
+          ),
+
+          //
+          // SliverFillRemaining(        // 剩余补充内容TabBarView
+          //   child: TabBarView(
+          //     controller: this.tabController,
+          //     children: <Widget>[
+          //       Center(child: Text('Content of Home')),
+          //       Center(child: Text('Content of Profile')),
+          //     ],
+          //   ),
+          // ),
+
+          // gridview
+          SliverPadding(
+            padding: const EdgeInsets.all(8.0),
+            sliver: SliverGrid(
+              //Grid
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2, //Grid按两列显示
+                mainAxisSpacing: 10.0,
+                crossAxisSpacing: 10.0,
+                childAspectRatio: 1,
+              ),
+              delegate: SliverChildBuilderDelegate(
+                (BuildContext context, int index) {
+                  //创建子widget
+                  return Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: <Widget>[
+                        // 当某子布局需要沿着主轴填充高度时使用，外部包裹Expanded属性
+                        Expanded(
+                          child: Container(
+                            width: 300,
+                            height: 300,
+                            color: Colors.green,
+                            child: Image.asset("assets/life/life_tab_vip_icon.png"),
+                          ),
+                        ),
+                        Text('主标题'),
+                        Text('积分'),
+                      ]);
+                },
+                childCount: 60,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
+  }
+
+  List<Widget> getListChildren() {
+    return [new Text("第一条:"), new Text("第二条"), new Text("第三条")];
   }
 
   // 构建 header
@@ -168,64 +250,27 @@ class _zyylivepageState extends State<zyylivepage> {
   }
 }
 
+class _SilverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SilverAppBarDelegate(this._tabBar);
 
-//     return Scaffold(
-//         body: Stack(
-//           children:<Widget> [
-//             MediaQuery.removePadding(  // 用来去除顶部状态栏
-//               removeTop: true,
-//               context: context,
-//               child: NotificationListener(  // 用来检测滚动距离
-//                 // ignore: missing_return
-//                 onNotification: (scrollNotification) {
-//                   if(scrollNotification != null && scrollNotification is ScrollUpdateNotification &&
-//                       scrollNotification.depth == 0){
-//                     _onScroll(scrollNotification.metrics.pixels);
-//                     return true;
-//                   }
-//                   return false;
-//                 },
-//                 child: ListView(
-//                   children: <Widget>[
-//                     Container(
-//                       height: 250,
-//                       child: Swiper(
-//                         itemCount: _imageurls.length,
-//                         autoplay: true,
-//                         itemBuilder: (BuildContext context,int index){
-//                           return Image.network(
-//                             _imageurls[index],
-//                             fit: BoxFit.fill,
-//                           );
-//                         },
-//                         pagination: SwiperPagination(),
-//                       ),
-//                     ),
-//                     Container(
-//                       height: 800,
-//                       child: ListTile(
-//                         title: Text('Hello World'),
-//                       ),
-//                     )
-//                   ],
-//                 ),
-//               ),
-//             ),
-//
-//
-//             Opacity(
-//               opacity: appBarAlpha,
-//               child: Container(
-//                 height: 60,
-//                 decoration: BoxDecoration(color: Colors.red),
-//                 child: Center(
-//                   child: Padding(padding: EdgeInsets.only(top: 20),
-//                     child: Text('首页'),),
-//                 ),
-//               ),
-//             )
-//           ],
-//         )
-//     );
-//   //}
-// }
+  final TabBar _tabBar;
+
+  @override
+  double get minExtent => _tabBar.preferredSize.height;
+
+  @override
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return new Container(
+      child: _tabBar,
+    );
+  }
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
+    return false;
+  }
+}
